83f0e1f0a479e85c18fa62b41c663ed275021512
Ronald J Nagata Jr <junzo@Ronalds-MBP.home>
2019-04-01 19:28:30 -1000

    First Solution

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,7 @@
 'use strict';
 module.exports = function() {
-  
+  const randomNumber = Math.floor(Math.random() * Math.floor(1000000));
+  return function secretNumber(){
+    return randomNumber;
+  }
 };
